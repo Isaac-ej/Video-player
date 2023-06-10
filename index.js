6943669f89fbaf822ae7ba7a10f6f4b5d723f24a
Isaac-ej <ejamaisaac2@gmail.com>
2023-06-10 14:35:02 +0300

    check linters/webhint errors

--- index.js
+++
@@ -1,5 +0,0 @@
-import('node-fetch').then((fetch) => {
-  // use fetch() here
-}).catch((err) => {
-  // handle error here
-});
---
+++ request-async.js
@@ -0,0 +1,7 @@
+import("node-fetch")
+  .then((fetch) => {
+    // use fetch() here
+  })
+  .catch((err) => {
+    // handle error here
+  });
